8d1ef24275cc3beb7222b50911341f521e0bc559
okapyy <m9_17g_suki@yahoo.co.jp>
2020-09-10 20:26:28 +0900

    user viewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'index', to: 'coodinates#index'
+  root to: 'coodinates#index'
+  resources :coodinates
 end
